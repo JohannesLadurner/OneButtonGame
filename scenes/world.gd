88e0394ca28530872ffd3f3709ce3a6f043bada3
JohannesLadurner <jo8@hotmail.de>
2024-03-10 22:26:18 +0100

    fix: slower gameplay

--- scenes/world.gd
+++ scenes/world.gd
@@ -2,15 +2,15 @@ extends Node2D
 
 var data = []
 func _ready():
-	$AudioStreamPlayer.stream = load("res://Jetta-I_d-Love-to-Change-the-World-_Matstubs-Remix_.wav")
-	data = AudioToWaveform.generate($AudioStreamPlayer.stream)
+	$AudioStreamPlayer.stream = load("res://maldita.wav")
+	data = AudioToWaveform.generate($AudioStreamPlayer.stream, 50)
 	$AudioStreamPlayer.play()
 	queue_redraw()
 	
 func _process(delta):
-	$Camera2D.position.x += delta*200
+	$Camera2D.position.x += delta*100
 
 func _draw():
 	for i in data.size():
-		draw_line(Vector2(i*2, 500), Vector2(i*2, 200-data[i]),Color.GREEN,2.0)
+		draw_line(Vector2(i*2, 500), Vector2(i*2, 300-data[i]), Color.GREEN,2.0)
 	
