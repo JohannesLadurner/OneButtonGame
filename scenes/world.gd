bf2505af30b6c759f615b31bdd8a142984013adb
JohannesLadurner <jo8@hotmail.de>
2024-03-10 22:01:42 +0100

    feat: added drawing of wave + camera

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
+var data = []
 func _ready():
-	$AudioStreamPlayer.stream = load("res://Golden Haze - Detious.wav")
-	var data = AudioToWaveform.generate($AudioStreamPlayer.stream)
-	print("here")
+	$AudioStreamPlayer.stream = load("res://Jetta-I_d-Love-to-Change-the-World-_Matstubs-Remix_.wav")
+	data = AudioToWaveform.generate($AudioStreamPlayer.stream)
+	$AudioStreamPlayer.play()
+	queue_redraw()
+	
+func _process(delta):
+	$Camera2D.position.x += delta*200
+
+func _draw():
+	for i in data.size():
+		draw_line(Vector2(i*2, 500), Vector2(i*2, 200-data[i]),Color.GREEN,2.0)
+	
