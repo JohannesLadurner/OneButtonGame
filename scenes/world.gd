2f5bd41af6b0be9b3fd99b1fbb9b7af5365b53af
JohannesLadurner <jo8@hotmail.de>
2024-03-10 22:57:39 +0100

    fix: added music synchronization

--- scenes/world.gd
+++ scenes/world.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
 var data = []
+var points_per_second = 50
 func _ready():
 	$AudioStreamPlayer.stream = load("res://maldita.wav")
-	data = AudioToWaveform.generate($AudioStreamPlayer.stream, 50)
+	data = AudioToWaveform.generate($AudioStreamPlayer.stream, points_per_second)
 	$AudioStreamPlayer.play()
 	queue_redraw()
 	
 func _process(delta):
-	$Camera2D.position.x += delta*100
+	$Camera2D.position.x += delta*(points_per_second*2)
 
 func _draw():
 	for i in data.size():
-		draw_line(Vector2(i*2, 500), Vector2(i*2, 300-data[i]), Color.GREEN,2.0)
+		if i % 50 == 0:
+			draw_line(Vector2(576+i*2, 648), Vector2(576+i*2, 600-data[i]), Color.RED,2.0)
+		else:
+			draw_line(Vector2(576+i*2, 648), Vector2(576+i*2, 600-data[i]), Color.GREEN,2.0)
 	
