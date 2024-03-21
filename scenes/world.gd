dcdc94d09770a1a5db71df8696c527a39003c857
JohannesLadurner <jo8@hotmail.de>
2024-03-21 22:12:56 +0100

    feat: added trail feedback (not balanced), scaled player, added song for testing

--- scenes/world.gd
+++ scenes/world.gd
@@ -4,7 +4,7 @@ extends Node2D
 class DataPoint:
 	var x: int
 	var y: int
-	var distance: int
+	var player_distance: int = -1
 
 var data_points = []
 var max_height = 500
@@ -37,6 +37,7 @@ func _process(delta):
 	$Camera2D.position.x = pos_x + camera_offset
 	$Sprite2D.position.x = pos_x
 	$Player.position.x = pos_x
+	data_points[data_index].player_distance = abs($Player.position.y - data_points[data_index].y)
 	$WaveClearedViewportContainer.position.x = pos_x - $WaveClearedViewportContainer.size.x - 5
 	$WaveClearedViewportContainer.queue_redraw()
 
@@ -62,11 +63,19 @@ func _draw():
 func _on_wave_cleared_viewport_container_draw():
 	var index = data_index
 	var offset = $Sprite2D.position.x
-	var draw_width = $WaveClearedViewportContainer.size.x-300
+	var draw_width = $WaveClearedViewportContainer.size.x+25
 	while offset - data_points[index].x < draw_width && index > 0:
+		var color
+		
+		#Use color depending on how near the player got
+		if data_points[index].player_distance == -1: color = Color.WHITE #No data available
+		elif data_points[index].player_distance < 50: color = Color.GREEN
+		elif data_points[index].player_distance < 100: color = Color.ORANGE
+		else: color = Color.RED
+		
 		var from = Vector2($WaveClearedViewportContainer.size.x+5-(offset-data_points[index].x), data_points[index].y)
 		var to = Vector2($WaveClearedViewportContainer.size.x+5-(offset-data_points[index-1].x), data_points[index-1].y)
-		$WaveClearedViewportContainer.draw_line(from, to, Color.GREEN, 3.0, true)
+		$WaveClearedViewportContainer.draw_line(from, to, color, 3.0, true)
 		index = index - 1
 
 func _generate_data_points(heights: Array):
@@ -86,6 +95,5 @@ func _generate_data_points(heights: Array):
 		var data_point = DataPoint.new()
 		data_point.x = x
 		data_point.y = 600-heights[i]
-		data_point.distance = distance
 		data_points.push_back(data_point)
 	return data_points
