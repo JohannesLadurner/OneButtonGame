96791b79c57e20675a7a72138f4595baf13445d7
Lukas-12 <lukas.buchsi@gmail.com>
2024-03-22 13:03:49 +0100

    feat: Player: Add lerp effect

--- Player.gd
+++ Player.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+const FOLLOW_SPEED = 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,6 +8,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var mouse_position = get_viewport().get_mouse_position()
-	position.y = mouse_position.y
+	#var mouse_position = get_viewport().get_mouse_position()
+	var mouse_position = get_local_mouse_position()
+	#position.y = mouse_position.y
+	$Sprite2D.position.y = $Sprite2D.position.lerp(mouse_position, delta * FOLLOW_SPEED).y
 	
