c99d721b5fbe82a1246c58fb32a625610d894888
JohannesLadurner <jo8@hotmail.de>
2024-03-27 09:26:25 +0100

    fix: fixed player position, added bounds

--- Player.gd
+++ Player.gd
@@ -1,5 +1,5 @@
 extends Node2D
-const FOLLOW_SPEED = 4.0
+const FOLLOW_SPEED = 6.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,8 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#var mouse_position = get_viewport().get_mouse_position()
-	var mouse_position = get_local_mouse_position()
-	#position.y = mouse_position.y
-	$Sprite2D.position.y = $Sprite2D.position.lerp(mouse_position, delta * FOLLOW_SPEED).y
-	
+	var mouse_position = get_global_mouse_position()
+	var new_height = position.lerp(mouse_position, delta * FOLLOW_SPEED).y
+	if new_height >= 0 and new_height <= 648:
+		position.y = new_height
