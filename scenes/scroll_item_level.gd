8e5e2e56a11de9577e9ad2c273b0149792af29af
JohannesLadurner <jo8@hotmail.de>
2024-06-11 20:24:21 +0200

    feat: added song duration for level selection

--- scenes/scroll_item_level.gd
+++ scenes/scroll_item_level.gd
@@ -6,6 +6,8 @@ signal selected
 func set_song(song: SongHandler.Song):
 	_song = song
 	$Title.text = song.get_title()
+	var duration = floor(load(song.get_stream_path()).get_length()) as int
+	$Duration.text = str(duration/60, ":", "%.2d" % (duration % 60))
 	var score = GameState.get_highscores(song.get_id()).get_score(GameState.gameplay_properties.get_difficulty())
 	$Highscore.text = str("Highscore: ", score)
 
