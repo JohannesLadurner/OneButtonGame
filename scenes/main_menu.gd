2184c7ff13511d8cef1b420d36db878489c309d9
JohannesLadurner <jo8@hotmail.de>
2024-06-12 15:38:00 +0200

    feat: added small character animation in main menu + hint

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var character_sprite = $SelectCharacterButton/Character
+var character_rotate_right = true
+var character_rotation_speed = 0.4
+var character_rotation_degrees = 15
 
 func _ready():
 	var character = CharacterHandler.get_character(GameState.selected_character_id)
@@ -8,6 +11,15 @@ func _ready():
 		character = CharacterHandler.get_all_characters()[0] #Just pick the first character if none was found
 		GameState.selected_character_id = character.get_id()
 	character_sprite.texture = load(character.get_texture_path())
+	
+func _process(delta):
+	if character_sprite.rotation_degrees >= character_rotation_degrees:
+		character_rotate_right = false
+	if character_sprite.rotation_degrees <= -character_rotation_degrees:
+		character_rotate_right = true
+	if character_rotate_right: character_sprite.rotate(character_rotation_speed*delta)
+	else: character_sprite.rotate(-character_rotation_speed*delta)
+		
 
 func _on_play_button_pressed():
 	FadeTransition.change_scene("res://scenes/song_selection.tscn")
